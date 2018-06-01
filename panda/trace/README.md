8a1abb3e725f86c0574a99221de4f98dd3e1fb31
s0i37 <s0i37@work>
2018-06-01 19:08:15 +0500

    added memory tracing in panda/trace

--- panda/trace/README.md
+++ panda/trace/README.md
@@ -1,3 +1,5 @@
 echo trace >> panda/plugins/config.panda
 
 make M=panda/plugins/trace
+
+./i386-softmmu/qemu-system-i386 -replay record_name -m 2047M -os windows-32-7 -panda trace:asid=$((0x19498000)),to=$((0x80000000))
--- panda/trace/trace.c
+++ panda/trace/trace.c
@@ -5,9 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define VERSION "0.10"
+#define VERSION "0.11"
 #define MAX_OPCODE_SIZE 15
 
+
+bool translate_callback(CPUState *env, target_ulong pc);
+int exec_callback(CPUState *env, target_ulong pc);
+int virt_mem_read(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf);
+int virt_mem_write(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf);
+
+/*======POSSIBLE CALLBACKS======
 bool translate_callback(CPUState *env, target_ulong pc);
 int exec_callback(CPUState *env, target_ulong pc);
 
@@ -20,6 +27,7 @@ int phys_mem_before_read(CPUState *env, target_ulong pc, target_ulong addr, targ
 int phys_mem_before_write(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf);
 int phys_mem_after_read(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf);
 int phys_mem_after_write(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf);
+*/
 
 bool init_plugin(void *);
 void uninit_plugin(void *);
@@ -64,139 +72,10 @@ void add_asid(uint64_t asid)
 }
 */
 
-
 bool translate_callback(CPUState *env, target_ulong pc)
 {
 	return true;
 }
-
-/*
-int virt_mem_before_read(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size)
-{
-#ifdef TARGET_I386
-	//CPUState * cpu = first_cpu;
-	CPUArchState * cpu = (CPUArchState *)env->env_ptr;
-	if( (uint64_t)( (CPUX86State *)cpu )->cr[3] != asid )
-		return 0;
-	fprintf( out_file, "read [0x%08x,0x%08x]:0x%08x(%u)->?\n", 
-		(unsigned int)pc,
-		(unsigned int)( (CPUX86State *)cpu )->eip, 
-		(unsigned int)addr, 
-		(unsigned int)size );
-#endif
-	return 0;
-}
-int virt_mem_before_write(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf)
-{
-#ifdef TARGET_I386
-	//CPUState * cpu = first_cpu;
-	CPUArchState * cpu = (CPUArchState *)env->env_ptr;
-	if( (uint64_t)( (CPUX86State *)cpu )->cr[3] != asid )
-		return 0;
-	fprintf( out_file, "write [0x%08x,0x%08x]:0x%08x(%u)<-%08X\n", 
-		(unsigned int)pc,
-		(unsigned int)( (CPUX86State *)cpu )->eip, 
-		(unsigned int)addr, 
-		(unsigned int)size, 
-		*((unsigned int *)buf) );
-#endif
-	return 0;
-}
-*/
-
-int virt_mem_after_read(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf)
-{
-#ifdef TARGET_I386
-	unsigned int i;
-	//CPUState * cpu = first_cpu;
-	CPUArchState * cpu = (CPUArchState *)env->env_ptr;
-	if( asid == 0 || (uint64_t)( (CPUX86State *)cpu )->cr[3] != asid )
-		return 0;
-	fprintf( out_file, "r [0x%08x]: 0x%08x(%u) -> ", (unsigned int)pc, (unsigned int)addr, (unsigned int)size );
-	for(i = 0; i < size; i++)
-		fprintf( out_file, "%02X", * (unsigned char*) ( (unsigned long)buf + i ) );
-	fprintf( out_file, "\n" );
-#endif
-	return 0;
-}
-
-int virt_mem_after_write(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf)
-{
-#ifdef TARGET_I386
-	unsigned int i;
-	//CPUState * cpu = first_cpu;
-	CPUArchState * cpu = (CPUArchState *)env->env_ptr;
-	if( asid == 0 || (uint64_t)( (CPUX86State *)cpu )->cr[3] != asid )
-		return 0;
-	fprintf( out_file, "w [0x%08x]: 0x%08x(%u) <- ", (unsigned int)pc, (unsigned int)addr, (unsigned int)size );
-	for(i = 0; i < size; i++)
-		fprintf( out_file, "%02X", * (unsigned char*) ( (unsigned long)buf + i ) );
-	fprintf( out_file, "\n" );
-#endif
-	return 0;
-}
-
-/*
-int phys_mem_before_read(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size)
-{
-#ifdef TARGET_I386
-	//CPUState * cpu = first_cpu;
-	CPUArchState * cpu = (CPUArchState *)env->env_ptr;
-	if( (uint64_t)( (CPUX86State *)cpu )->cr[3] != asid )
-		return 0;
-	fprintf( out_file, "read phys before [0x%08x]0x%08x (%u)->?\n", 
-		(unsigned int)( (CPUX86State *)cpu )->eip, 
-		(unsigned int)addr, 
-		(unsigned int)size );
-#endif
-	return 0;
-}
-int phys_mem_before_write(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf)
-{
-#ifdef TARGET_I386
-	//CPUState * cpu = first_cpu;
-	CPUArchState * cpu = (CPUArchState *)env->env_ptr;
-	if( (uint64_t)( (CPUX86State *)cpu )->cr[3] != asid )
-		return 0;
-	fprintf( out_file, "write phys before [0x%08x]0x%08x (%u)->%08X\n", 
-		(unsigned int)( (CPUX86State *)cpu )->eip, 
-		(unsigned int)addr, 
-		(unsigned int)size, 
-		*((unsigned int *)buf) );
-#endif
-	return 0;
-}
-int phys_mem_after_read(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf)
-{
-#ifdef TARGET_I386
-	//CPUState * cpu = first_cpu;
-	CPUArchState * cpu = (CPUArchState *)env->env_ptr;
-	if( (uint64_t)( (CPUX86State *)cpu )->cr[3] != asid )
-		return 0;
-	fprintf( out_file, "read phys after [0x%08x]0x%08x (%u)->%08X\n", 
-	   	(unsigned int)( (CPUX86State *)cpu )->eip, 
-	   	(unsigned int)addr, 
-	   	(unsigned int)size, 
-	   	*((unsigned int *)buf) );
-#endif
-    return 0;
-}
-int phys_mem_after_write(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf)
-{
-#ifdef TARGET_I386
-	//CPUState * cpu = first_cpu;
-	CPUArchState * cpu = (CPUArchState *)env->env_ptr;
-	if( (uint64_t)( (CPUX86State *)cpu )->cr[3] != asid )
-		return 0;
-	fprintf( out_file, "write phys after [0x%08x]0x%08x (%u)->%08X\n", 
-	  	(unsigned int)( (CPUX86State *)cpu )->eip, 
-	   	(unsigned int)addr, 
-	   	(unsigned int)size, 
-	   	*((unsigned int *)buf) );
-#endif
-    return 0;
-}
-*/
 
 int exec_callback(CPUState *env, target_ulong pc)
 {
@@ -210,7 +89,7 @@ int exec_callback(CPUState *env, target_ulong pc)
 		return 0;
 	if( (from == 0 && to == 0) || ((unsigned int)pc >= from && (unsigned int)pc <= to) )
 	{
-		fprintf( out_file, "0x%08x:%x {", (unsigned int)pc, ( (struct X86CPU *)cpu )->thread_id );
+		fprintf( out_file, "0x%08x:0x%x {", (unsigned int)pc, ( (struct X86CPU *)cpu )->thread_id );
 		panda_virtual_memory_read(env, pc, buf, MAX_OPCODE_SIZE+1);
 		for(i = 0; i < MAX_OPCODE_SIZE; i++)
 			fprintf( out_file, "%02X", ( (unsigned char *)buf )[i] );
@@ -238,6 +117,74 @@ int exec_callback(CPUState *env, target_ulong pc)
 	return 0;
 }
 
+int virt_mem_read(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf)
+{
+#ifdef TARGET_I386
+	//CPUState * cpu = first_cpu;
+	CPUArchState * cpu = (CPUArchState *)env->env_ptr;
+	if( asid == 0 || (uint64_t)( (CPUX86State *)cpu )->cr[3] != asid )
+		return 0;
+	if( (from == 0 && to == 0) || ((unsigned int)pc >= from && (unsigned int)pc <= to) )
+	{
+		fprintf( out_file, "0x%08x:0x%x [0x%08x] -> ", 
+			(unsigned int)pc,
+			( (struct X86CPU *)cpu )->thread_id,
+			(unsigned int)addr );
+		switch( (unsigned int)size )
+		{
+			case 1:
+				fprintf( out_file, "0x%02x\n", *(unsigned char *)buf );
+				break;
+			case 2:
+				fprintf( out_file, "0x%04x\n", *(unsigned short *)buf );
+				break;
+			case 4:
+				fprintf( out_file, "0x%08x\n", *(unsigned int *)buf );
+				break;
+			case 8:
+				fprintf( out_file, "0x%08x", *(unsigned int *)buf );
+				fprintf( out_file, "%08x\n", *( ((unsigned int *)buf) + 1 ) );
+				break;
+		}
+	}
+#endif
+	return 0;
+}
+
+int virt_mem_write(CPUState *env, target_ulong pc, target_ulong addr, target_ulong size, void *buf)
+{
+#ifdef TARGET_I386
+	//CPUState * cpu = first_cpu;
+	CPUArchState * cpu = (CPUArchState *)env->env_ptr;
+	if( asid == 0 || (uint64_t)( (CPUX86State *)cpu )->cr[3] != asid )
+		return 0;
+	if( (from == 0 && to == 0) || ((unsigned int)pc >= from && (unsigned int)pc <= to) )
+	{
+		fprintf( out_file, "0x%08x:0x%x [0x%08x] <- ", 
+			(unsigned int)pc,
+			( (struct X86CPU *)cpu )->thread_id,
+			(unsigned int)addr );
+		switch( (unsigned int)size )
+		{
+			case 1:
+				fprintf( out_file, "0x%02x\n", *(unsigned char *)buf );
+				break;
+			case 2:
+				fprintf( out_file, "0x%04x\n", *(unsigned short *)buf );
+				break;
+			case 4:
+				fprintf( out_file, "0x%08x\n", *(unsigned int *)buf );
+				break;
+			case 8:
+				fprintf( out_file, "0x%08x", *(unsigned int *)buf );
+				fprintf( out_file, "%08x\n", *( ((unsigned int *)buf) + 1 ) );
+				break;
+		}
+	}
+#endif
+	return 0;
+}
+
 panda_arg_list *args;
 bool init_plugin(void *self)
 {
@@ -249,19 +196,7 @@ bool init_plugin(void *self)
 	printf("[*] trace " VERSION "\n");
 	panda_enable_memcb();
 	panda_enable_precise_pc();
-	/*
-	if(args)
-		for(i = 0; i < args->nargs; i++)
-		{
-			if( strncmp(args->list[i].key, "file", 4) == 0 )
-				out_file_name = args->list[i].value;
-			else if( strncmp(args->list[i].key, "asid", 4) == 0 )
-			{
-				asid = (uint64_t) atol( args->list[i].key );
-				printf("[+] select CR3=%lx\n", asid);
-			}
-		}
-	*/
+
 	out_file_name = panda_parse_string_opt(args, "outfile", "trace.txt", "output filename, where will be store trace information in humanable form");
 	asid = panda_parse_uint64_opt(args, "asid", 0, "only this CR3 (default all address spaces)");
 	from = panda_parse_uint64_opt(args, "from", 0, "start address for tracing");
@@ -272,39 +207,26 @@ bool init_plugin(void *self)
 		printf("[+] from: 0x%08lx, to: 0x%08lx\n", from, to);
 
 	out_file = fopen(out_file_name, "w");
+	fprintf(out_file, "EIP:THREAD_ID {OPCODE} EAX,ECX,EDX,EBX,ESP,EBP,ESI,EDI\n");
+	fprintf(out_file, "EIP:THREAD_ID [MEMORY] -> READED_VALUE\n");
+	fprintf(out_file, "EIP:THREAD_ID [MEMORY] <- WRITED_VALUE\n");
 
 	pcb.insn_translate = translate_callback;
 	panda_register_callback(self, PANDA_CB_INSN_TRANSLATE, pcb);
 	pcb.insn_exec = exec_callback;
 	panda_register_callback(self, PANDA_CB_INSN_EXEC, pcb);
 
-	//pcb.virt_mem_before_read = virt_mem_before_read;
-	//panda_register_callback(self, PANDA_CB_VIRT_MEM_BEFORE_READ, pcb);
-	//pcb.virt_mem_before_write = virt_mem_before_write;
-	//panda_register_callback(self, PANDA_CB_VIRT_MEM_BEFORE_WRITE, pcb);
-
-	/*
-	pcb.virt_mem_after_read = virt_mem_after_read;
+	pcb.virt_mem_after_read = virt_mem_read;
 	panda_register_callback(self, PANDA_CB_VIRT_MEM_AFTER_READ, pcb);
-	pcb.virt_mem_after_write = virt_mem_after_write;
-	panda_register_callback(self, PANDA_CB_VIRT_MEM_AFTER_WRITE, pcb);
-	*/
+	pcb.virt_mem_before_write = virt_mem_write;
+	panda_register_callback(self, PANDA_CB_VIRT_MEM_BEFORE_WRITE, pcb);
 
-	//pcb.phys_mem_before_read = phys_mem_before_read;
-	//panda_register_callback(self, PANDA_CB_PHYS_MEM_BEFORE_READ, pcb);
-	//pcb.phys_mem_before_write = phys_mem_before_write;
-	//panda_register_callback(self, PANDA_CB_PHYS_MEM_BEFORE_WRITE, pcb);
-
-	//pcb.phys_mem_after_read = phys_mem_after_read;
-    //panda_register_callback(self, PANDA_CB_PHYS_MEM_AFTER_READ, pcb);
-    //pcb.phys_mem_after_write = phys_mem_after_write;
-    //panda_register_callback(self, PANDA_CB_PHYS_MEM_AFTER_WRITE, pcb);
 	return 1;
 }
 
 void uninit_plugin(void *self)
 {
-	printf("[done]\n");
+	printf("[*] done\n");
 	panda_free_args(args);
 	fflush(out_file);
 	fclose(out_file);
