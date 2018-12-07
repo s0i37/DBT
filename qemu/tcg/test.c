b66ef78088b04233cece0014c2c14a88f02a004b
s0i37 <s0i37@ya.ru>
2018-12-07 15:25:39 +0500

    Update test.c

--- qemu/tcg/test.c
+++ qemu/tcg/test.c
@@ -32,26 +32,78 @@
 
 #include "tcg-plugin.h"
 
+#include <capstone/capstone.h>
+
+csh cs_handle;
+cs_insn *insn;
+size_t count = 0;
+uint64_t ins_count = 0;
+
 static void pre_tb_helper_code(const TCGPluginInterface *tpi,
                                TPIHelperInfo info, uint64_t address,
                                uint64_t data1, uint64_t data2,
                                const TranslationBlock* tb)
 {
-    //if( ( (CPUX86State *) tpi->tcg_ctx->cpu->env_ptr )->cr[3] == 0x00a49000 && tpi->low_pc > 0x80000000 )
-    //if( tpi->low_pc > 0x80000000 )
-        fprintf(tpi->output, "0x%08lx\n", tpi->low_pc);
-    //fprintf(tpi->output, "0x%08lx\n", (uint64_t)tpi->tcg_ctx->cpu);
+    ins_count++;
+    if(ins_count%1000000 != 0)
+      return;
+
+    if(count)
+      fprintf(tpi->output, "0x%08x: %s %s\n", insn->address, insn->mnemonic, insn->op_str);
+    fprintf(tpi->output, "EIP: 0x%08lx\n", address);
+    CPUArchState *cpu = tpi_current_cpu_arch(tpi);
+    fprintf(tpi->output, "EAX: 0x%08x ", (unsigned int) ( (CPUX86State *)cpu )->regs[R_EAX] );
+    fprintf(tpi->output, "ECX: 0x%08x ", (unsigned int) ( (CPUX86State *)cpu )->regs[R_ECX] );
+    fprintf(tpi->output, "EDX: 0x%08x ", (unsigned int) ( (CPUX86State *)cpu )->regs[R_EDX] );
+    fprintf(tpi->output, "EBX: 0x%08x\n", (unsigned int) ( (CPUX86State *)cpu )->regs[R_EBX] );
+    fprintf(tpi->output, "ESP: 0x%08x ", (unsigned int) ( (CPUX86State *)cpu )->regs[R_ESP] );
+    fprintf(tpi->output, "EBP: 0x%08x ", (unsigned int) ( (CPUX86State *)cpu )->regs[R_EBP] );
+    fprintf(tpi->output, "ESI: 0x%08x ", (unsigned int) ( (CPUX86State *)cpu )->regs[R_ESI] );
+    fprintf(tpi->output, "EDI: 0x%08x\n", (unsigned int) ( (CPUX86State *)cpu )->regs[R_EDI] );
+    fprintf(tpi->output, "CR1: 0x%08x ", (unsigned int) ( (CPUX86State *)cpu )->cr[0] );
+    fprintf(tpi->output, "CR2: 0x%08x ", (unsigned int) ( (CPUX86State *)cpu )->cr[1] );
+    fprintf(tpi->output, "CR3: 0x%08x ", (unsigned int) ( (CPUX86State *)cpu )->cr[2] );
+    fprintf(tpi->output, "CR4: 0x%08x\n", (unsigned int) ( (CPUX86State *)cpu )->cr[3] );
+    fprintf(tpi->output, "================================================================\n");
+    //sleep(1);
+}
+
+/*
+static void pre_tb_helper_data(const TCGPluginInterface *tpi,
+                                          TPIHelperInfo info, uint64_t address,
+                                          uint64_t *data1, uint64_t *data2,
+                                          const TranslationBlock* tb)
+{
+
+}
+*/
+
+static void after_gen_opc(const TCGPluginInterface *tpi, const TPIOpCode *tpi_opcode)
+{
+    cs_insn *insns;
+
+    /* SEGFAULTs happen sometimes */
+    //count = cs_disasm(cs_handle, (void *)(intptr_t)tpi_guest_ptr(tpi, tpi_opcode->pc), 16, tpi_opcode->pc, 1, &insns);
+    if(count)
+      insn = &insns[0];
 }
 
 static void cpus_stopped(const TCGPluginInterface *tpi)
 {
-    
+    /* because (monitor) loadvm state   leads cpu stop */
+    //cs_free(insn, 1);
+    //cs_close(&cs_handle);
 }
 
 void tpi_init(TCGPluginInterface *tpi)
 {
     TPI_INIT_VERSION_GENERIC(tpi);
 
+    if (cs_open(CS_ARCH_X86, CS_MODE_64, &cs_handle) != CS_ERR_OK)
+      return;
+
     tpi->pre_tb_helper_code = pre_tb_helper_code;
+    //tpi->pre_tb_helper_data = pre_tb_helper_data;
+    tpi->after_gen_opc = after_gen_opc;
     tpi->cpus_stopped = cpus_stopped;
 }
